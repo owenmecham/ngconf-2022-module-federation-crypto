a380eb825c9c20c591754e366686a07801f0c622
owenmecham <owen.mecham@gmail.com>
2022-07-30 19:26:56 -0600

    -feat working module federation

--- projects/shell/webpack.config.js
+++ projects/shell/webpack.config.js
@@ -1,13 +1,17 @@
-const { shareAll, withModuleFederationPlugin } = require('@angular-architects/module-federation/webpack');
+const { share, withModuleFederationPlugin } = require('@angular-architects/module-federation/webpack');
 
 module.exports = withModuleFederationPlugin({
 
   remotes: {
-    "mfe1": "http://localhost:4200/remoteEntry.js",    
+    "mfe1": "http://localhost:3000/remoteEntry.js",
   },
 
-  shared: {
-    ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
-  },
+  shared:
+  share({
+    "@angular/core": { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+        "@angular/common": { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+        "@angular/router": { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+        "@angular/material": { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+  }),
 
 });
